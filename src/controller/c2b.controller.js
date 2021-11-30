3232355ddd30491557a2e06e62fbbd365777af5e
Francisco Andre <franciscodamana@gmail.com>
2021-11-30 10:55:21 +0200

    c2b feito

--- src/controller/c2b.controller.js
+++ src/controller/c2b.controller.js
@@ -1,31 +1,33 @@
 var axios = require('axios').default;
-const jwt = require('jsonwebtoken');
 const config = require('../config');
 
 
-async function token() {
-    const token = jwt.sign({ apiKey: config.apiKey, publicKey: config.publicKey }, (config.apiKey + config.publicKey), {
-        expiresIn: 180
-    });
-    return token
-}
+
 
 async function c2b(request, response) {
-    const tok = await token();
+    const { numero, valor } = request.body;
+    if (!numero)
+        return response.status(400).send({
+            message: 'Digite o número'
+        })
+    if (!valor)
+        return response.status(400).send({
+            message: 'Digite o valor'
+        })
 
     const api = axios.create({
-        baseURL: config.url
+        baseURL: config.urlC2B
     });
 
     const data = {
-        input_TransactionReference: 'T12344C',
-        input_CustomerMSISDN: '258846461323',
-        input_Amount: '10',
-        input_ThirdPartyReference: '11114',
-        input_serviceProviderCode: '171717'
+        input_TransactionReference: config.input_TransactionReference,
+        input_CustomerMSISDN: numero,
+        input_Amount: valor,
+        input_ThirdPartyReference: config.input_ThirdPartyReference,
+        input_serviceProviderCode: config.input_serviceProviderCode
     }
 
-    api.post("/ipg/v1x/c2bPayment/singleStage/ HTTP/1.1", {
+    api.post("/ipg/v1x/c2bPayment/singleStage/", {
 
         input_TransactionReference: data.input_TransactionReference,
         input_CustomerMSISDN: data.input_CustomerMSISDN,
@@ -35,7 +37,7 @@ async function c2b(request, response) {
 
     }, {
         headers: {
-            Authorization: 'Bearer ' + tok,
+            Authorization: config.auth,
             Origin: config.origin,
 
         }
